418c9ec153cdeba9f81df170f4d36b72879c5c79
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2022-06-11 01:46:04 +0430

    adding generator imports

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,5 @@
     Make the importing much shorter
 """
 from .ner import SpacyNER
-
+from .gpt2 import get_special_tokens, get_model, get_tokenier, CommentGeneratorDataset
+from .utils import make_generation
