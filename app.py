83ec103e6182d8d7c040f145195fe1a0d1f8a6c8
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2022-06-12 16:11:02 +0430

    adding postgresql db to app

--- app.py
+++ app.py
@@ -3,14 +3,26 @@ import json
 from configuration import ModelConfig, DeployConfig
 from datahandler import DataReader
 from generator import TextGenerator
-from src import make_generation
+from src import (make_generation,
+                 CommentGeneratorDataset,
+                 SpacyNER,
+                 PostgreSQLDatabase)
+import keys
+
 
 APP = Flask(__name__)
 
 MODEL_CONFIG = ModelConfig().get_args()
 DEPLOY_CONFIG = DeployConfig().get_args()
 
-MODEL = TextGenerator(MODEL_CONFIG, DEPLOY_CONFIG)
+DATABASE = PostgreSQLDatabase(database=keys.DATABASE,
+                              user=keys.USER, 
+                              password=keys.PASSWORD, 
+                              host=keys.HOST, 
+                              port=keys.PORT)
+
+GENERATOR_MODEL = TextGenerator(MODEL_CONFIG, DEPLOY_CONFIG)
+NER_MODEL = SpacyNER(model_name=MODEL_CONFIG.spacy_ner_model, ners=MODEL_CONFIG.ners)
 
 @APP.route("/ping")
 def ping():
@@ -31,10 +43,14 @@ def gen_text():
     """
     data = request.get_json(force=True)
     text, num_sequences = data['text'], data['num-sequences']
-    predicts = make_generation(model = MODEL, text = text, num_sequences=num_sequences)    
+    predicts = make_generation(generator_model=GENERATOR_MODEL, 
+                               ner_model=NER_MODEL, 
+                               dataset=CommentGeneratorDataset,
+                               input_text=text, 
+                               database=DATABASE, 
+                               num_sequences=num_sequences)
     result = {'result':predicts}
     return Response(json.dumps(result, ensure_ascii=False), content_type='application/json')
-
 
 if __name__=="__main__":
     APP.run(debug=False, host='0.0.0.0')
