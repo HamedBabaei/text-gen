d90019961963f06ac70726c55f33c5481f6fc5c2
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2022-06-12 16:11:12 +0430

    adding db to import

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,4 @@
 from .ner import SpacyNER
 from .gpt2 import get_special_tokens, get_model, get_tokenier, CommentGeneratorDataset
 from .utils import make_generation
+from .database import PostgreSQLDatabase
